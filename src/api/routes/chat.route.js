9cffab78dfb4a6b7474259a19f4655aebc6d04cd
hailt <luonghai5622@gmail.com>
2024-10-10 14:55:38 +0700

    Thêm báo lỗi

--- src/api/routes/chat.route.js
+++ src/api/routes/chat.route.js
@@ -3,5 +3,12 @@ const route = express.Router();
 const chatService = require("../controllers/chat.controller");
 
 route.post("/", chatService.message)
+// api dùng để test báo lỗi
+route.get('/hiho', (req, res, next) => {
+    let err = new Error('Route not found!')
+    err.status = 404
+    next(err)
+})
+// </>
 
 module.exports = route;
--- src/server.js
+++ src/server.js
@@ -1,34 +1,42 @@
-require('dotenv').config();
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io')
-const SocketService = require('./api/services/socket');
+require("dotenv").config();
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const SocketService = require("./api/services/socket");
 
 const app = express();
 const port = process.env.PORT;
 
-app.use(express.json())
+app.use(express.json());
 app.use(
-    express.urlencoded({
-        extended: true
-    })
-)
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST']
-    }
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 global._io = io;
-global._io.on('connection', SocketService.connection);
+global._io.on("connection", SocketService.connection);
 
-app.get('/', (req, res) => {
-    res.json('Hello World!');
+app.get("/", (req, res) => {
+  res.json("Hello World!");
 });
-app.use('/chat', require('./api/routes/chat.route'));
+app.use("/chat", require("./api/routes/chat.route"));
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    code: err.status,
+    message: err.message,
+  });
+});
 
 server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
