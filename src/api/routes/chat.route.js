2fde64578b0c25dffb78cfd4bb748e21e675bcb7
hailt <luonghai5622@gmail.com>
2024-10-10 15:08:51 +0700

    Thêm báo lỗi

--- src/api/routes/chat.route.js
+++ src/api/routes/chat.route.js
@@ -2,13 +2,13 @@ const express = require("express");
 const route = express.Router();
 const chatService = require("../controllers/chat.controller");
 
-route.post("/", chatService.message)
+route.post("/", chatService.message);
 // api dùng để test báo lỗi
-route.get('/hiho', (req, res, next) => {
-    let err = new Error('Route not found!')
-    err.status = 404
-    next(err)
-})
+route.get("/hiho", (req, res, next) => {
+  let err = new Error("Route not found!");
+  err.status = 404;
+  next(err);
+});
 // </>
 
 module.exports = route;
