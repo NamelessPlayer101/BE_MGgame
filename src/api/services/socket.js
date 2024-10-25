d3d5d66fc855c69dc7ffd9e0913ba1c01cb8d2e9
hailt <luonghai5622@gmail.com>
2024-10-25 09:42:17 +0700

    Temp

--- src/api/services/socket.js
+++ src/api/services/socket.js
@@ -1,9 +1,13 @@
-class SocketService {
+const backEndPlayers = {};
+
+class SocketService {  
   connection(socket) {
     console.log(`${socket.id} connected`);
 
     socket.on("disconnect", (reason) => {
       console.log(`reason: ${reason}`);
+      delete backEndPlayers[socket.id];
+      _io.emit('updatePlayers', backEndPlayers);
     });
 
     // Ping
@@ -23,12 +27,36 @@ class SocketService {
     });
 
     // Game
-    socket.on("game", () => {
+    socket.on("game", (data) => {
+      const playerInit = JSON.parse(data);
+      backEndPlayers[socket.id] = {
+        ...playerInit,
+        radius: 15,
+      }
+
       _io.emit(
         "game",
         JSON.stringify({
+          code: 200,
           id: socket.id,
-          message: 'Hehe',
+          data: backEndPlayers,
+        })
+      );
+    });
+
+    socket.on("updatePlayers", (data) => {
+      const playerInit = JSON.parse(data);
+      backEndPlayers[socket.id] = {
+        ...playerInit,
+        radius: 15,
+      }
+
+      _io.emit(
+        "updatePlayers",
+        JSON.stringify({
+          code: 200,
+          id: socket.id,
+          data: backEndPlayers,
         })
       );
     });
